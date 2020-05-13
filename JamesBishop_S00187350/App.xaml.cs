d2f6a26ebf724206d5c83b8ae150d4dbc1740794
james bishop <s00187350@mail.itsligo.ie>
2020-05-13 10:03:51 +0100

    Add project files.

---
+++ JamesBishop_S00187350/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JamesBishop_S00187350
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
