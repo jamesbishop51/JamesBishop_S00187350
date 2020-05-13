26f315d3deb57e30e38c7f028eff315982cd203a
james bishop <s00187350@mail.itsligo.ie>
2020-05-13 10:19:35 +0100

    added the class with the properties

---
+++ JamesBishop_S00187350/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JamesBishop_S00187350
+{
+    class Phone
+    {
+        public string Name { get; set; }
+        public int Price{ get; set; }
+        public string OperatingSystem{ get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+
+
+
+
+    }
+}
